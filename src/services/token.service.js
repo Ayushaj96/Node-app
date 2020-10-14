b3bd95d5d2d1706946763f84e1b8e4ee31ecc17c
Ayush jain <ayushaj007@gmail.com>
2020-10-14 13:57:58 +0530

    Update token.service.js

--- src/services/token.service.js
+++ src/services/token.service.js
@@ -2,13 +2,6 @@ const jwt = require('jsonwebtoken');
 const moment = require('moment');
 const config = require('../config/config');
 
-/**
- * Generate token
- * @param {ObjectId} userId
- * @param {Moment} expires
- * @param {string} [secret]
- * @returns {string}
- */
 const generateToken = (userId, expires, secret = config.JWT_SECRET) => {
   const payload = {
     sub: userId,
@@ -18,18 +11,47 @@ const generateToken = (userId, expires, secret = config.JWT_SECRET) => {
   return jwt.sign(payload, secret);
 };
 
-/**
- * Generate auth tokens
- * @param {User} user
- * @returns {Promise<Object>}
- */
 const generateAuthTokens = async (user) => {
   const accessTokenExpires = moment().add(config.JWT_ACCESS_EXPIRATION_MINUTES, 'minutes');
   const accessToken = generateToken(user.GLN, accessTokenExpires);
   return accessToken;
 };
 
+const verifyAuthToken = async(req, res, next) => {
+
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    let response = {};
+
+    if (token == null) {
+        response.status = httpStatus.UNAUTHORIZED;
+        response.message = httpStatus[httpStatus.UNAUTHORIZED];
+        response.data = "Token not found";
+        return next(res.status(response.status).send(response));
+    } else {
+
+        jwt.verify(token, config.JWT_SECRET,(err, user) => {
+
+            if (err) {
+                response.status = httpStatus.FORBIDDEN;
+                response.message = httpStatus[httpStatus.FORBIDDEN];
+
+                if (err instanceof jwt.TokenExpiredError) {
+                    response.data = "Token expired";
+                } else if (err instanceof jwt.JsonWebTokenError) {
+                    response.data = "Invalid token";
+                }
+                return next(res.status(response.status).send(response));
+
+            } else {
+                req.user = user;
+                next();
+            }
+        });
+    }
+}
+
 module.exports = {
-  generateToken,
   generateAuthTokens,
+  verifyAuthToken,
 };
